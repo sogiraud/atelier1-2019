479e6ec4a556f390aa8a98452cdb5c2b28b41795
sogiraud <55701460+sogiraud@users.noreply.github.com>
2019-09-23 17:12:00 +0200

    Add files via upload

---
+++ TD BIOSTAT.R
@@ -0,0 +1,27 @@
+2 + 3
+8-12
+14 * 25
+-3/10
+x<-2
+x
+y<-seq(from=0,to=10^6,by=1)
+y
+x <- 2
+x = 2
+2 -> x
+x
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+mean(tailles)#moyenne
+var(tailles)#variance
+sd(tailles)#ecar-type
+summary(tailles)#resume
+tailles^2
+sin(tailles)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(tailles)
+length(poids)
+tailles.m<-tailles/100
+tailles.m
+imc<-poids/tailles.m^2
+imc
